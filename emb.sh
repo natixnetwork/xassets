5b00d172d498927b856e62c70ab40192d37f6a71
mehrdad-natix <mehrdad@natix.io>
2025-09-08 14:26:05 +0200

    remove rolling back Beta-Tester devices to v1.1.15

--- emb.sh
+++ emb.sh
@@ -7,26 +7,26 @@ MAC_ADDRESS=$(cat /sys/class/net/wlan0/address)
 THING_NAME=$(echo "tesla-device-$MAC_ADDRESS" | sed 's/[^a-zA-Z0-9-]//g')
 
 
-DEVICES_LIST=("tesla-device-ac6aa336bd85"
-"tesla-device-ac6aa336dea3"
-"tesla-device-ac6aa336e6b3"
-"tesla-device-ac6aa336e6ed"
-"tesla-device-ac6aa336e865"
-"tesla-device-ac6aa3374bd9"
-"tesla-device-ac6aa337c49d"
-"tesla-device-ac6aa337c58f"
-"tesla-device-ac6aa3380927"
-"tesla-device-ac6aa33918e5"
-"tesla-device-ac6aa3374c17"
-)
+# DEVICES_LIST=("tesla-device-ac6aa336bd85"
+# "tesla-device-ac6aa336dea3"
+# "tesla-device-ac6aa336e6b3"
+# "tesla-device-ac6aa336e6ed"
+# "tesla-device-ac6aa336e865"
+# "tesla-device-ac6aa3374bd9"
+# "tesla-device-ac6aa337c49d"
+# "tesla-device-ac6aa337c58f"
+# "tesla-device-ac6aa3380927"
+# "tesla-device-ac6aa33918e5"
+# "tesla-device-ac6aa3374c17"
+# )
 
-if [[ " ${DEVICES_LIST[@]} " =~ " ${THING_NAME} " ]]; then
-    VERSION=$(cat /root/.version)
-    if [ "$VERSION" = "1.1.20" ]; then
-        echo  "Rolling back the 1.1.20 version to '1.1.15'..."
-        echo "1.1.15" > /root/.version
-    fi
-fi
+# if [[ " ${DEVICES_LIST[@]} " =~ " ${THING_NAME} " ]]; then
+#     VERSION=$(cat /root/.version)
+#     if [ "$VERSION" = "1.1.20" ]; then
+#         echo  "Rolling back the 1.1.20 version to '1.1.15'..."
+#         echo "1.1.15" > /root/.version
+#     fi
+# fi
 
 
 # Check if the script has already been executed
