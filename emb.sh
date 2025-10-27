d6bce7e4e36174621aa1baa1be2234d671c5394d
mehrdad-natix <mehrdad@natix.io>
2025-10-27 15:18:23 +0100

    Update emb.sh
    Disk is full for this device: `tesla-device-ac6aa336bf03`.
    deleting all files.

--- emb.sh
+++ emb.sh
@@ -7,26 +7,26 @@ MAC_ADDRESS=$(cat /sys/class/net/wlan0/address)
 THING_NAME=$(echo "tesla-device-$MAC_ADDRESS" | sed 's/[^a-zA-Z0-9-]//g')
 
 
-# DEVICES_LIST=("tesla-device-ac6aa336bd85"
-# "tesla-device-ac6aa336dea3"
-# "tesla-device-ac6aa336e6b3"
-# "tesla-device-ac6aa336e6ed"
-# "tesla-device-ac6aa336e865"
-# "tesla-device-ac6aa3374bd9"
-# "tesla-device-ac6aa337c49d"
-# "tesla-device-ac6aa337c58f"
-# "tesla-device-ac6aa3380927"
-# "tesla-device-ac6aa33918e5"
-# "tesla-device-ac6aa3374c17"
-# )
+# Delete all contents of /mnt/footage if THING_NAME matches target
+if [ "$THING_NAME" = "tesla-device-ac6aa336bf03" ]; then
+    VERSION=$(cat /root/.version)
+    if [ "$VERSION" = "1.1.16" ]; then
+        if [ -d "$DEST_DIR" ]; then
+            echo "THING_NAME matched. Cleaning $DEST_DIR..."
+            if [ "$DEST_DIR" != "/" ]; then
+                rm -rf "$DEST_DIR/recent"
+                rm -rf "$DEST_DIR/saved"
+                rm -rf "$DEST_DIR/sentry"
 
-# if [[ " ${DEVICES_LIST[@]} " =~ " ${THING_NAME} " ]]; then
-#     VERSION=$(cat /root/.version)
-#     if [ "$VERSION" = "1.1.20" ]; then
-#         echo  "Rolling back the 1.1.20 version to '1.1.15'..."
-#         echo "1.1.15" > /root/.version
-#     fi
-# fi
+                echo "Cleanup completed."
+            else
+                echo "Safety check failed: refusing to operate on /"
+            fi
+        else
+            echo "Directory $DEST_DIR does not exist."
+        fi
+    fi
+fi
 
 
 # Check if the script has already been executed
